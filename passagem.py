6c2eb1c6f446f4469d8701c5f5ad0158f77f1953
milena sam <milenafurutashishito@gmail.com>
2024-05-19 00:26:00 -0300

    encontrar_melhor_voo de ida e de volta (substituiçãod e codigo repetido por função genérica)

--- passagem.py
+++ passagem.py
@@ -33,14 +33,60 @@ class Data:
     def ano(self, ano):
         self._ano = ano
 
-    def eh_maior_ou_igual(self, outra_data):
+    def eh_maior(self, outra_data):
         if self._ano < outra_data.ano:
             return False
         if self._mes < outra_data.mes:
             return False
-        if self._dia < outra_data.dia:
+        if self._dia <= outra_data.dia:
             return False
         return True
+    
+    def eh_menor(self, outra_data):
+        if self._ano > outra_data.ano:
+            return False
+        if self._mes > outra_data.mes:
+            return False
+        if self._dia >= outra_data.dia:
+            return False
+        return True
+    
+    def eh_igual(self, outra_data):
+        if self._ano != outra_data.ano or self._mes != outra_data.mes or self._dia != outra_data.dia:
+            return False
+        return True
+
+    def somar_subtrair_dias(self, dias_a_somar):
+        dias_por_mes = {
+            1: 31,
+            2: 28,
+            3: 31,
+            4: 30,
+            5: 31,
+            6: 30,
+            7: 31,
+            8: 31,
+            9: 30,
+            10: 31,
+            11: 30,
+            12: 31
+        }
+
+        dia = self._dia + dias_a_somar
+        mes = self._mes
+        ano = self._ano
+        
+        if dia <= 0:
+            mes = self._mes - 1
+            if mes == 0:
+                mes = 12
+                ano = self._ano -1
+            dia = dias_por_mes.get(mes) - dia
+        
+        self._dia = dia
+        self._mes = mes
+        self._ano = ano
+
 
 class Voo:
     def __init__(self, numero, aeroport_origem_cod, aeroport_destino_cod, data, preco):
@@ -92,20 +138,7 @@ class Voo:
 
 # Aqui em baixo fica a sua solucao
 
-dias_por_mes = {
-    1: 31,
-    2: 28,
-    3: 31,
-    4: 30,
-    5: 31,
-    6: 30,
-    7: 31,
-    8: 31,
-    9: 30,
-    10: 31,
-    11: 30,
-    12: 31
-}
+
 
 voos = []
 passagem_props = {
@@ -130,6 +163,18 @@ def encontrar_voos_com_aeroporto_de_origem(codigo):
         if voos[i].aeroport_origem_cod == codigo:
             res.append(voos[i])
     return res
+
+def encontrar_melhor_voo(array_voos, data_min, data_max):
+    melhor_voo = array_voos[0]
+
+    for v in array_voos:
+        if v.data.eh_maior(data_max) or v.data.eh_menor(data_min):
+            continue
+
+        if v.preco < melhor_voo.preco:
+            melhor_voo = v
+
+    return melhor_voo
 
 def registrar():
     for p in passagem_props.keys():
@@ -176,31 +221,51 @@ for linha in stdin:
         data_inicio_ferias = Data(dia, mes, ano)
         dia, mes, ano = formatar_data(data_fim_ferias)
         data_fim_ferias = Data(dia, mes, ano)
+        data_limite_passagem_ida = data_fim_ferias
+        data_limite_passagem_ida.somar_subtrair_dias(-4)
         
-        dia_limite = data_fim_ferias.dia - 3
-        mes_limite = data_fim_ferias.mes
-        ano_limite = data_fim_ferias.ano
+        # dia_limite_ida = data_fim_ferias.dia - 3
+        # mes_limite_ida = data_fim_ferias.mes
+        # ano_limite_ida = data_fim_ferias.ano
         
-        if dia_limite <= 0:
-            mes_limite = data_fim_ferias.mes - 1
-            if mes_limite == 0:
-                mes_limite = 12
-                ano_limite = data_fim_ferias.ano -1
-            dia_limite = dias_por_mes.get(mes_limite) - dia_limite
+        # if dia_limite_ida <= 0:
+        #     mes_limite_ida = data_fim_ferias.mes - 1
+        #     if mes_limite_ida == 0:
+        #         mes_limite_ida = 12
+        #         ano_limite_ida = data_fim_ferias.ano -1
+        #     dia_limite_ida = dias_por_mes.get(mes_limite_ida) - dia_limite_ida
         
-        data_limite_passagem = Data(dia_limite,mes_limite,ano_limite)
+        # data_limite_passagem_ida = Data(dia_limite_ida,mes_limite_ida,ano_limite_ida)
         
         voos_com_aeroporto_do_jarzinho = encontrar_voos_com_aeroporto_de_origem(jarzinho_aeroporto)
-        melhor_voo = voos_com_aeroporto_do_jarzinho[0]
-        
-        for v in voos_com_aeroporto_do_jarzinho:
-            if v.data.eh_maior_ou_igual(data_inicio_ferias) == False and v.data.eh_maior_ou_igual(data_limite_passagem) == True:
-                continue
 
-            if v.preco < melhor_voo.preco:
-                melhor_voo = v
+        melhor_voo_ida = encontrar_melhor_voo(voos_com_aeroporto_do_jarzinho, data_inicio_ferias, data_limite_passagem_ida)
+
+        # melhor_voo_ida = voos_com_aeroporto_do_jarzinho[0]
         
-        print(melhor_voo.numero)
-        print(melhor_voo.numero)
+        # for v in voos_com_aeroporto_do_jarzinho:
+        #     if v.data.eh_maior_ou_igual(data_inicio_ferias) == False and v.data.eh_maior_ou_igual(data_limite_passagem_ida) == True:
+        #         continue
+
+        #     if v.preco < melhor_voo_ida.preco:
+        #         melhor_voo_ida = v
+
+        data_limite_passagem_volta = data_limite_passagem_ida
+        data_limite_passagem_volta.somar_subtrair_dias(4)
+        
+        voos_retorno_viajem = encontrar_voos_com_aeroporto_de_origem(melhor_voo_ida.aeroport_destino_cod)
+
+        melhor_voo_volta = encontrar_melhor_voo(voos_retorno_viajem, data_limite_passagem_volta, data_fim_ferias)
+        # melhor_voo_volta = voos_retorno_viajem[0]
+
+        # for v in voos_retorno_viajem:
+        #     if v.data.eh_maior_ou_igual(data_inicio_ferias) == False and v.data.eh_maior_ou_igual(data_limite_passagem_volta) == True:
+        #         continue
+
+        #     if v.preco < melhor_voo_volta.preco:
+        #         melhor_voo_volta = v
+        
+        print(melhor_voo_ida.numero)
+        print(melhor_voo_volta.numero)
 
 
